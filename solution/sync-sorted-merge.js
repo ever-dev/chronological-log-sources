b2b83364bcf12e6d66e55451f2ec005ff82ad6df
ever-dev <everdev0923@gmail.com>
2021-05-27 14:35:28 -0400

    Simplify the sync sorted merge

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -2,28 +2,25 @@
 
 // Print all entries, across all of the sources, in chronological order.
 
+const logSort = (log1, log2) => {
+  return log1.value.date < log2.value.date
+    ? -1
+    : log1.value.date > log2.value.date
+    ? 1
+    : 0;
+};
+
 module.exports = (logSources, printer) => {
-  const lastLogs = logSources.map((logSource) => ({
+  let lastLogs = logSources.map((logSource) => ({
     source: logSource,
     value: logSource.pop(),
   }));
 
-  while (lastLogs.some((lastLog) => lastLog.source.drained === false)) {
-    const newLog = lastLogs.reduce((log, lastLog) => {
-      if (
-        lastLog.source.drained === false &&
-        (log === null ||
-          log.value === false ||
-          log.value.date > lastLog.value.date)
-      ) {
-        return lastLog;
-      }
-      return log;
-    }, null);
-
-    printer.print(newLog.value);
-
-    newLog.value = newLog.source.pop();
+  while (lastLogs.length > 0) {
+    lastLogs.sort(logSort);
+    printer.print(lastLogs[0].value);
+    lastLogs[0].value = lastLogs[0].source.pop();
+    lastLogs = lastLogs.filter((log) => log.source.drained === false && log.source.value !== false);
   }
 
   return console.log("Sync sort complete.");
