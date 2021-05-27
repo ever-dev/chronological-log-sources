edf93a6cb0c045d84a458759e5f0d5a7482012c5
ever-dev <everdev0923@gmail.com>
2021-05-27 14:17:10 -0400

    Implement chronological sort

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -3,7 +3,37 @@
 // Print all entries, across all of the *async* sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
+    const lastLogs = logSources.map((logSource) => ({
+      source: logSource,
+      value: logSource.popAsync(),
+    }));
+
+    while (lastLogs.some((lastLog) => lastLog.source.drained === false)) {
+      await Promise.all(lastLogs.map((lastLog) => lastLog.value)).then(
+        (logValues) => {
+          const newLog = logValues.reduce(
+            (acc, logValue, index) => {
+              if (
+                lastLogs[index].source.drained === false &&
+                (acc.log === null ||
+                  acc.log === false ||
+                  acc.log.date > logValue.date)
+              ) {
+                return { index, log: logValue };
+              }
+              return acc;
+            },
+            { index: 0, log: null }
+          );
+
+          printer.print(newLog.log);
+
+          lastLogs[newLog.index].value = lastLogs[newLog.index].source.pop();
+        }
+      );
+    }
+
     resolve(console.log("Async sort complete."));
   });
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,5 +3,28 @@
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  const lastLogs = logSources.map((logSource) => ({
+    source: logSource,
+    value: logSource.pop(),
+  }));
+
+  while (lastLogs.some((lastLog) => lastLog.source.drained === false)) {
+    const newLog = lastLogs.reduce((log, lastLog) => {
+      if (
+        lastLog.source.drained === false &&
+        (log === null ||
+          log.value === false ||
+          log.value.date > lastLog.value.date)
+      ) {
+        return lastLog;
+      }
+      return log;
+    }, null);
+
+    printer.print(newLog.value);
+
+    newLog.value = newLog.source.pop();
+  }
+
   return console.log("Sync sort complete.");
 };
